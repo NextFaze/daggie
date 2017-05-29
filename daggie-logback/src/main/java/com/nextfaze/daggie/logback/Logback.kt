e0c0e652ff4ea4cecff2bfbb21bfe14fbfb37956
Ben Tilbrook <btilbrook@nextfaze.com>
2017-05-29 14:10:48 +0930

    Move uncaught exception logging into Slf4jModule

--- daggie-logback/src/main/java/com/nextfaze/daggie/logback/Logback.kt
+++ daggie-logback/src/main/java/com/nextfaze/daggie/logback/Logback.kt
@@ -13,7 +13,6 @@ import ch.qos.logback.core.Appender
 import ch.qos.logback.core.AppenderBase
 import com.nextfaze.daggie.Early
 import com.nextfaze.daggie.Initializer
-import com.nextfaze.daggie.slf4j.logger
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.ElementsIntoSet
@@ -55,7 +54,6 @@ typealias LogbackAppender = AppenderBase<ILoggingEvent>
             logback: Logback,
             logCatAppender: LogCatAppender
     ): Initializer<Application> = {
-        Thread.setDefaultUncaughtExceptionHandler(debugLoggingUncaughtExceptionHandler(it, Thread.getDefaultUncaughtExceptionHandler()))
         logback.addAppender(logCatAppender.apply {
             context = loggerContext
             start()
@@ -105,26 +103,6 @@ private fun androidLogbackLoggerContext(): LoggerContext {
     applyLoggerContextPerformanceFix()
     return LoggerFactory.getILoggerFactory() as LoggerContext
 }
-
-/**
- * This will allow us to see the full stack track in LogCat.
- * The default LogCat output `E/AndroidRuntime﹕ FATAL EXCEPTION: main ...` gets cut off when > ~4k characters.
- * However we only do this in Debug as calling `log.error()` may also send the logs to other crash logging services.
- * But as an uncaught exception, this has already been done (thus they will be doubled - but for debuggable builds only)
- * @see ApplicationInfo.FLAG_DEBUGGABLE
- */
-private fun debugLoggingUncaughtExceptionHandler(context: Context, wrapped: Thread.UncaughtExceptionHandler) =
-        object : Thread.UncaughtExceptionHandler {
-
-            private val log = logger()
-
-            override fun uncaughtException(thread: Thread, ex: Throwable) {
-                if (context.isDebuggable) {
-                    log.error("FATAL EXCEPTION", ex)
-                }
-                wrapped.uncaughtException(thread, ex)
-            }
-        }
 
 private val Context.isDebuggable get() = (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
 
---
+++ daggie-slf4j/src/main/java/com/nextfaze/daggie/slf4j/Slf4jModule.kt
@@ -0,0 +1,41 @@
+package com.nextfaze.daggie.slf4j
+
+import android.app.Application
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import com.nextfaze.daggie.Early
+import com.nextfaze.daggie.Initializer
+import dagger.Module
+import dagger.Provides
+import dagger.multibindings.IntoSet
+import javax.inject.Singleton
+
+/** Provides bindings to initialize SLF4J, including logging uncaught exceptions. */
+@Module class Slf4jModule {
+    @Provides @IntoSet @Singleton @Early internal fun initializer(): Initializer<Application> = {
+        Thread.setDefaultUncaughtExceptionHandler(debugLoggingUncaughtExceptionHandler(it,
+                Thread.getDefaultUncaughtExceptionHandler()))
+    }
+}
+
+/**
+ * This will allow us to see the full stack track in LogCat.
+ * The default LogCat output `E/AndroidRuntime﹕ FATAL EXCEPTION: main ...` gets cut off when > ~4k characters.
+ * However we only do this in Debug as calling `log.error()` may also send the logs to other crash logging services.
+ * But as an uncaught exception, this has already been done (thus they will be doubled - but for debuggable builds only)
+ * @see ApplicationInfo.FLAG_DEBUGGABLE
+ */
+private fun debugLoggingUncaughtExceptionHandler(context: Context, wrapped: Thread.UncaughtExceptionHandler) =
+        object : Thread.UncaughtExceptionHandler {
+
+            private val log = logger()
+
+            override fun uncaughtException(thread: Thread, ex: Throwable) {
+                if (context.isDebuggable) {
+                    log.error("FATAL EXCEPTION", ex)
+                }
+                wrapped.uncaughtException(thread, ex)
+            }
+        }
+
+private val Context.isDebuggable get() = (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
