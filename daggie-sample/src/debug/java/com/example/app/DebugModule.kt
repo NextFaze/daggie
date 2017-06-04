f84d08f281b96a3f6e47e3db443b71ef1418cc81
Ben Tilbrook <btilbrook@nextfaze.com>
2017-06-05 09:24:47 +0930

    Add Stetho module

--- daggie-sample/src/debug/java/com/example/app/DebugModule.kt
+++ daggie-sample/src/debug/java/com/example/app/DebugModule.kt
@@ -4,15 +4,16 @@ import com.nextfaze.daggie.devproxy.DevProxyConfig
 import com.nextfaze.daggie.devproxy.DevProxyModule
 import com.nextfaze.daggie.leakcanary.LeakCanaryModule
 import com.nextfaze.daggie.sample.BuildConfig
+import com.nextfaze.daggie.stetho.StethoModule
 import dagger.Module
 import dagger.Provides
 
 /** Provides additional debug-only bindings, such as those supplying dev menus or debugging tools. */
 @Module(includes = arrayOf(
         DevProxyModule::class,
-        LeakCanaryModule::class
+        LeakCanaryModule::class,
+        StethoModule::class
 //        StrictModeModule::class,
-//        StethoModule::class,
 ))
 class DebugModule {
     @Provides internal fun devProxyConfig() = DevProxyConfig(BuildConfig.DEV_PROXY_HOST, BuildConfig.DEV_PROXY_PORT)
---
+++ daggie-stetho/src/main/java/com/nextfaze/daggie/stetho/StethoModule.kt
@@ -0,0 +1,14 @@
+package com.nextfaze.daggie.stetho
+
+import android.app.Application
+import com.facebook.stetho.Stetho
+import com.nextfaze.daggie.Initializer
+import dagger.Module
+import dagger.Provides
+import dagger.multibindings.IntoSet
+
+@Module class StethoModule {
+    @Provides @IntoSet fun initializer(): Initializer<Application> = {
+        Stetho.initializeWithDefaults(it)
+    }
+}
