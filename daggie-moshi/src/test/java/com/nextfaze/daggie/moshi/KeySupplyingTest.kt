316ca77a3e0910d92d6df54e3e14e385a0adbc72
Ben Tilbrook <btilbrook@nextfaze.com>
2018-02-05 11:04:02 +1030

    Require declaring annotation @RequiresKey on models #23

--- daggie-moshi/src/test/java/com/nextfaze/daggie/moshi/KeySupplyingTest.kt
+++ daggie-moshi/src/test/java/com/nextfaze/daggie/moshi/KeySupplyingTest.kt
@@ -4,76 +4,59 @@ package com.nextfaze.daggie.moshi
 
 import com.google.common.truth.Truth.assertThat
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types.newParameterizedType
+import org.junit.Before
 import org.junit.Test
 
 class KeySupplyingTest {
 
-    private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .add(KeySupplyingJsonAdapterFactory())
-        .build()
+    private lateinit var factory: KeySupplyingJsonAdapterFactory
+
+    private lateinit var moshi: Moshi
 
     private val adapterOfListOfItem
-        get() = moshi.adapter<List<Item>>(newParameterizedType(List::class.java, Item::class.java))
-    private val adapterOfSetOfItem
-        get() = moshi.adapter<Set<Item>>(newParameterizedType(Set::class.java, Item::class.java))
+        get() = moshi.adapter<List<Item>>(
+            newParameterizedType(List::class.java, Item::class.java)
+        )
     private val adapterOfCollectionOfItem
-        get() = moshi.adapter<Collection<Item>>(newParameterizedType(Collection::class.java, Item::class.java))
+        get() = moshi.adapter<Collection<Item>>(
+            newParameterizedType(Collection::class.java, Item::class.java)
+        )
     private val adapterOfMapOfItem
-        get() = moshi.adapter<Map<String, Item>>(newParameterizedType(Map::class.java, Any::class.java, Item::class.java))
+        get() = moshi.adapter<Map<String, Item>>(
+            newParameterizedType(Map::class.java, Any::class.java, Item::class.java)
+        )
 
-    @Test fun `read list of models from a map with their respective keys`() {
-        val input = mapOf(
-            "a" to mapOf("name" to "A"),
-            "b" to mapOf("name" to "B")
-        )
-        val output = adapterOfListOfItem.fromJsonValue(input)
-        assertThat(output).isEqualTo(
-            listOf(
-                Item("a", "A"),
-                Item("b", "B")
-            )
-        )
+    @Before fun setUp() {
+        factory = KeySupplyingJsonAdapterFactory()
+        moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .add(factory)
+            .build()
     }
 
-    @Test fun `read set of models from a map with their respective keys`() {
-        val input = mapOf(
+    /* Read */
+
+    @Test fun `reads list of models with keys from a map`() {
+        val map = mapOf(
             "a" to mapOf("name" to "A"),
             "b" to mapOf("name" to "B")
         )
-        val output = adapterOfSetOfItem.fromJsonValue(input)
-        assertThat(output).isEqualTo(
-            setOf(
-                Item("a", "A"),
-                Item("b", "B")
-            )
-        )
+        assertThat(adapterOfListOfItem.fromJsonValue(map)).containsExactly(
+            Item("a", "A"),
+            Item("b", "B")
+        ).inOrder()
     }
 
-    @Test fun `read collection of models from a map with their respective keys`() {
-        val input = mapOf(
+    @Test fun `reads map of models with keys from a map`() {
+        val map = mapOf(
             "a" to mapOf("name" to "A"),
             "b" to mapOf("name" to "B")
         )
-        val output = adapterOfCollectionOfItem.fromJsonValue(input)
-        assertThat(output).isEqualTo(
-            listOf(
-                Item("a", "A"),
-                Item("b", "B")
-            )
-        )
-    }
-
-    @Test fun `read map of models from a map with their respective keys`() {
-        val input = mapOf(
-            "a" to mapOf("name" to "A"),
-            "b" to mapOf("name" to "B")
-        )
-        val output = adapterOfMapOfItem.fromJsonValue(input)
-        assertThat(output).isEqualTo(
+        assertThat(adapterOfMapOfItem.fromJsonValue(map)).isEqualTo(
             mapOf(
                 "a" to Item("a", "A"),
                 "b" to Item("b", "B")
@@ -81,19 +64,37 @@ class KeySupplyingTest {
         )
     }
 
-    @Test fun `read null list of models`() = assertThat(adapterOfListOfItem.fromJsonValue(null)).isNull()
+    @Test(expected = JsonDataException::class)
+    fun `fails to read a list of models with keys from a list`() {
+        val list = listOf(
+            mapOf("name" to "A"),
+            mapOf("name" to "B")
+        )
+        adapterOfListOfItem.fromJsonValue(list)
+    }
 
-    @Test fun `read null set of models`() = assertThat(adapterOfSetOfItem.fromJsonValue(null)).isNull()
+    @Test(expected = JsonDataException::class)
+    fun `fails to read a map of models with keys from a list`() {
+        val list = listOf(
+            mapOf("name" to "A"),
+            mapOf("name" to "B")
+        )
+        adapterOfMapOfItem.fromJsonValue(list)
+    }
 
-    @Test fun `read null collection of models`() = assertThat(adapterOfCollectionOfItem.fromJsonValue(null)).isNull()
+    @Test fun `does not attempt to read non-annotated type`() {
+        val type = newParameterizedType(List::class.java, ItemNonAnnotated::class.java)
+        assertThat(factory.create(type, emptySet(), moshi)).isNull()
+    }
 
-    @Test fun `write list of models to a map excluding the synthetic keys`() {
-        val input = listOf(
+    /* Write */
+
+    @Test fun `writes list of models with keys omitted to a map`() {
+        val list = listOf(
             Item("a", "A"),
             Item("b", "B")
         )
-        val output = adapterOfListOfItem.toJsonValue(input)
-        assertThat(output).isEqualTo(
+        assertThat(adapterOfListOfItem.toJsonValue(list)).isEqualTo(
             mapOf(
                 "a" to mapOf("name" to "A"),
                 "b" to mapOf("name" to "B")
@@ -101,41 +102,12 @@ class KeySupplyingTest {
         )
     }
 
-    @Test fun `write set of models to a map excluding the synthetic keys`() {
-        val input = setOf(
-            Item("a", "A"),
-            Item("b", "B")
-        )
-        val output = adapterOfSetOfItem.toJsonValue(input)
-        assertThat(output).isEqualTo(
-            mapOf(
-                "a" to mapOf("name" to "A"),
-                "b" to mapOf("name" to "B")
-            )
-        )
-    }
-
-    @Test fun `write collection of models to a map excluding the synthetic keys`() {
-        val input = listOf(
-            Item("a", "A"),
-            Item("b", "B")
-        )
-        val output = adapterOfCollectionOfItem.toJsonValue(input)
-        assertThat(output).isEqualTo(
-            mapOf(
-                "a" to mapOf("name" to "A"),
-                "b" to mapOf("name" to "B")
-            )
-        )
-    }
-
-    @Test fun `write map of models to a map excluding the synthetic keys`() {
-        val input = mapOf(
+    @Test fun `writes map of models with keys omitted to a map`() {
+        val map = mapOf(
             "a" to Item("a", "A"),
             "b" to Item("b", "B")
         )
-        val output = adapterOfMapOfItem.toJsonValue(input)
-        assertThat(output).isEqualTo(
+        assertThat(adapterOfMapOfItem.toJsonValue(map)).isEqualTo(
             mapOf(
                 "a" to mapOf("name" to "A"),
                 "b" to mapOf("name" to "B")
@@ -143,29 +115,30 @@ class KeySupplyingTest {
         )
     }
 
-    @Test fun `write null list of models`() = assertThat(adapterOfListOfItem.toJsonValue(null)).isNull()
+    /* Misc */
 
-    @Test fun `write null set of models`() = assertThat(adapterOfSetOfItem.toJsonValue(null)).isNull()
-
-    @Test fun `write null collection of models`() = assertThat(adapterOfCollectionOfItem.toJsonValue(null)).isNull()
-
-    @Test fun `read collection of nullable primitives from a map`() {
-        val input = mapOf("c" to "C", "d" to "D", "e" to null)
-        val adapter =
-            moshi.adapter<Collection<String?>>(newParameterizedType(Collection::class.java, String::class.java))
-        val output = adapter.fromJsonValue(input)
-        assertThat(output).isEqualTo(listOf("C", "D", null))
-    }
-
-    @Test(expected = IllegalArgumentException::class)
-    fun `special key throws if it would otherwise override a property from the input data`() {
+    @Test(expected = JsonDataException::class)
+    fun `presence of special key results in error if it would override a property from the input data`() {
         val input = mapOf("a" to mapOf("name" to "A", "_key" to "42"))
         adapterOfCollectionOfItem.fromJsonValue(input)
     }
 
+    @Test fun `reads and writes are null-safe`() {
+        assertThat(adapterOfListOfItem.toJsonValue(null)).isNull()
+        assertThat(adapterOfMapOfItem.toJsonValue(null)).isNull()
+        assertThat(adapterOfListOfItem.fromJsonValue(null)).isNull()
+        assertThat(adapterOfMapOfItem.fromJsonValue(null)).isNull()
+    }
+
+    @RequiresKey
     data class Item(
         @Json(name = "_key")
         val id: Any,
+        @Json(name = "name")
+        val name: String
+    )
+
+    data class ItemNonAnnotated(
         @Json(name = "name")
         val name: String
     )
