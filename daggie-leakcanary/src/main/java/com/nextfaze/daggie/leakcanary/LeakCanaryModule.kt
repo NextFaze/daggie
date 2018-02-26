e8da965c49ab1a43a8a68d2cb3e634e2419c646a
Alex Waters <awaters@nextfaze.com>
2018-02-26 15:48:26 +1030

    Don't install LeakCanary when built with JRebel

--- daggie-leakcanary/src/main/java/com/nextfaze/daggie/leakcanary/LeakCanaryModule.kt
+++ daggie-leakcanary/src/main/java/com/nextfaze/daggie/leakcanary/LeakCanaryModule.kt
@@ -6,9 +6,19 @@ import com.squareup.leakcanary.LeakCanary
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoSet
+import org.slf4j.LoggerFactory
 
 @Module class LeakCanaryModule {
+    private val log = LoggerFactory.getLogger(LeakCanaryModule::class.java.name)
+
     @Provides @IntoSet internal fun initializer(): Initializer<Application> = {
-        LeakCanary.install(it)
+        // Don't install LeakCanary when using JRebel
+        // It is rarely able to identify the source of the leak due to JRebel (shadow classes etc.) and just gets in the way
+        if (!this::class.java.classLoader::class.java.name.contains("jrebel")) {
+            LeakCanary.install(it)
+            log.debug("LeakCanary installed.")
+        } else {
+            log.debug("JRebel instance detected - skipping LeakCanary installation.")
+        }
     }
 }
