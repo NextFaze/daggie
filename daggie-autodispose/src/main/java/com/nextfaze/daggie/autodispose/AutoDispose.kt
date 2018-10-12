8d3984bb62f709f40ae33383b5ad401d28e860b6
Kane O'Riley <koriley@nextfaze.com>
2018-10-12 16:30:20 +1030

    PR feedback, code cleanup and improvements #33

--- daggie-autodispose/src/main/java/com/nextfaze/daggie/autodispose/AutoDispose.kt
+++ daggie-autodispose/src/main/java/com/nextfaze/daggie/autodispose/AutoDispose.kt
@@ -9,27 +9,80 @@ import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.nextfaze.daggie.Initializer
+import com.uber.autodispose.ScopeProvider
+import com.uber.autodispose.lifecycle.CorrespondingEventsFunction
+import com.uber.autodispose.lifecycle.LifecycleEndedException
 import com.uber.autodispose.lifecycle.LifecycleScopeProvider
+import com.uber.autodispose.lifecycle.LifecycleScopes
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoSet
+import io.reactivex.CompletableSource
+import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
 import java.lang.IllegalStateException
 
-private val lifecycleManager = AutoDisposeLifecycleManager()
-
 @Module
 class AutoDisposeModule {
-    @Provides @IntoSet internal fun lifecycleInitializer(): Initializer<Application> = { lifecycleManager.registerCallbacks(it) }
+    @Provides @IntoSet internal fun initializer(): Initializer<Application> = { LifecycleManager.registerCallbacks(it) }
 }
 
-fun Fragment.scope(untilEvent: FragmentEvent? = null): LifecycleScopeProvider<FragmentEvent> =
-    FragmentLifecycleProvider(lifecycleManager.lifecycle(this), untilEvent)
+enum class ActivityEvent {
+    ON_CREATE, ON_START, ON_RESUME, ON_PAUSE, ON_STOP, ON_DESTROY
+}
 
-fun Activity.scope(untilEvent: ActivityEvent? = null): LifecycleScopeProvider<ActivityEvent> =
-    ActivityLifecycleProvider(lifecycleManager.lifecycle(this), untilEvent)
+enum class FragmentEvent {
+    ON_ATTACH, ON_CREATE, ON_CREATE_VIEW, ON_START, ON_RESUME, ON_PAUSE, ON_STOP, ON_DESTROY_VIEW, ON_DESTROY, ON_DETACH
+}
 
-private class AutoDisposeLifecycleManager {
+fun Fragment.scope(untilEvent: FragmentEvent? = null): ScopeProvider =
+    LifecycleProvider(LifecycleManager.lifecycle(this), untilEvent, DEFAULT_FRAGMENT_CORRESPONDING_EVENTS)
+
+fun Activity.scope(untilEvent: ActivityEvent? = null): ScopeProvider =
+    LifecycleProvider(LifecycleManager.lifecycle(this), untilEvent, DEFAULT_ACTIVITY_CORRESPONDING_EVENTS)
+
+private val DEFAULT_ACTIVITY_CORRESPONDING_EVENTS = CorrespondingEventsFunction<ActivityEvent> { activityEvent ->
+    when (activityEvent) {
+        ActivityEvent.ON_CREATE -> ActivityEvent.ON_DESTROY
+        ActivityEvent.ON_START -> ActivityEvent.ON_STOP
+        ActivityEvent.ON_RESUME -> ActivityEvent.ON_PAUSE
+        ActivityEvent.ON_PAUSE -> ActivityEvent.ON_STOP
+        ActivityEvent.ON_STOP -> ActivityEvent.ON_DESTROY
+        else -> throw LifecycleEndedException("Cannot bind to Activity lifecycle after destroy.")
+    }
+}
+
+private val DEFAULT_FRAGMENT_CORRESPONDING_EVENTS = CorrespondingEventsFunction<FragmentEvent> { event ->
+    when (event) {
+        FragmentEvent.ON_ATTACH -> FragmentEvent.ON_DETACH
+        FragmentEvent.ON_CREATE -> FragmentEvent.ON_DESTROY
+        FragmentEvent.ON_CREATE_VIEW -> FragmentEvent.ON_DESTROY_VIEW
+        FragmentEvent.ON_START -> FragmentEvent.ON_STOP
+        FragmentEvent.ON_RESUME -> FragmentEvent.ON_PAUSE
+        FragmentEvent.ON_PAUSE -> FragmentEvent.ON_STOP
+        FragmentEvent.ON_STOP -> FragmentEvent.ON_DESTROY_VIEW
+        FragmentEvent.ON_DESTROY_VIEW -> FragmentEvent.ON_DESTROY
+        FragmentEvent.ON_DESTROY -> FragmentEvent.ON_DETACH
+        else -> throw LifecycleEndedException("Cannot bind to Fragment lifecycle after detach.")
+    }
+}
+
+private class LifecycleProvider<T> (
+    private val lifecycleEvents: BehaviorSubject<T>,
+    private val untilEvent: T? = null,
+    private val defaultCorrespondingEvents: CorrespondingEventsFunction<T>
+) : LifecycleScopeProvider<T> {
+    override fun lifecycle(): Observable<T> = lifecycleEvents.hide()
+
+    override fun correspondingEvents() =
+        untilEvent?.let { event -> CorrespondingEventsFunction<T> { event } } ?: defaultCorrespondingEvents
+
+    override fun peekLifecycle() = lifecycleEvents.value
+
+    override fun requestScope(): CompletableSource = LifecycleScopes.resolveScopeFromLifecycle(this)
+}
+
+private object LifecycleManager {
 
     private val fragments = mutableMapOf<Fragment, BehaviorSubject<FragmentEvent>>()
     private val activities = mutableMapOf<Activity, BehaviorSubject<ActivityEvent>>()
@@ -65,7 +118,7 @@ private class AutoDisposeLifecycleManager {
         override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) { /** Ignored */ }
     }
 
-    fun registerCallbacks(application: Application) = application.registerActivityLifecycleCallbacks(activityCallbacks)
+    internal fun registerCallbacks(application: Application) = application.registerActivityLifecycleCallbacks(activityCallbacks)
 
     internal fun lifecycle(fragment: Fragment) =
         fragments[fragment] ?: throw IllegalStateException("Attempting to bind to lifecycle for $fragment when no lifecycle is available")
