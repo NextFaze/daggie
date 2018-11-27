f211ceb983bcfdf87f410e94fbbb3103f191da56
Ben Tilbrook <btilbrook@nextfaze.com>
2018-11-28 09:10:03 +1030

    Fix lint warning

--- daggie-sample/src/main/java/com/example/app/CurrentUserView.kt
+++ daggie-sample/src/main/java/com/example/app/CurrentUserView.kt
@@ -3,14 +3,14 @@ package com.example.app
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
 import javax.inject.Inject
 
 @SuppressLint("SetTextI18n")
 class CurrentUserView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
-        TextView(context, attrs, defStyleAttr) {
+    AppCompatTextView(context, attrs, defStyleAttr) {
 
-    @Inject lateinit internal var user: User
+    @Inject lateinit var user: User
 
     init {
         userScopeInjector.inject(this)
