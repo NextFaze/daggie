b0ab604999b8f4daed51835060e62a39b458e42a
Ben Tilbrook <btilbrook@nextfaze.com>
2019-03-01 12:27:44 +1030

    Add filter(Not)Empty() overloads for Single, Maybe

--- daggie-rxjava2/src/main/java/com/nextfaze/daggie/rxjava2/Filtering.kt
+++ daggie-rxjava2/src/main/java/com/nextfaze/daggie/rxjava2/Filtering.kt
@@ -1,7 +1,9 @@
 package com.nextfaze.daggie.rxjava2
 
 import io.reactivex.Flowable
+import io.reactivex.Maybe
 import io.reactivex.Observable
+import io.reactivex.Single
 
 /** Filters a `Flowable` by only emitting `true` values. */
 fun Flowable<Boolean>.filterTrue(): Flowable<Boolean> = filter { it }
@@ -21,8 +23,20 @@ fun <C : Collection<*>> Flowable<C>.filterEmpty(): Flowable<C> = filter { it.isE
 /** Filters an `Observable` by only emitting empty collections. */
 fun <C : Collection<*>> Observable<C>.filterEmpty(): Observable<C> = filter { it.isEmpty() }
 
+/** Filters a `Single` by only emitting empty collections. */
+fun <C : Collection<*>> Single<C>.filterEmpty(): Maybe<C> = filter { it.isEmpty() }
+
+/** Filters a `Maybe` by only emitting empty collections. */
+fun <C : Collection<*>> Maybe<C>.filterEmpty(): Maybe<C> = filter { it.isEmpty() }
+
 /** Filters a `Flowable` by only emitting non-empty collections. */
 fun <C : Collection<*>> Flowable<C>.filterNotEmpty(): Flowable<C> = filter { it.isNotEmpty() }
 
 /** Filters an `Observable` by only emitting non-empty collections. */
 fun <C : Collection<*>> Observable<C>.filterNotEmpty(): Observable<C> = filter { it.isNotEmpty() }
+
+/** Filters a `Single` by only emitting non-empty collections. */
+fun <C : Collection<*>> Single<C>.filterNotEmpty(): Maybe<C> = filter { it.isNotEmpty() }
+
+/** Filters a `Maybe` by only emitting non-empty collections. */
+fun <C : Collection<*>> Maybe<C>.filterNotEmpty(): Maybe<C> = filter { it.isNotEmpty() }
