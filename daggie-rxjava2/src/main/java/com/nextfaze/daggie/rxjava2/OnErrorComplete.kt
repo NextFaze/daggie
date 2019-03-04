00d155f0ffff1eeaaddfbaa4f62c2b22fcca40f4
Ben Tilbrook <btilbrook@nextfaze.com>
2019-03-04 10:52:08 +1030

    Add Single.onErrorComplete() overload

--- daggie-rxjava2/src/main/java/com/nextfaze/daggie/rxjava2/OnErrorComplete.kt
+++ daggie-rxjava2/src/main/java/com/nextfaze/daggie/rxjava2/OnErrorComplete.kt
@@ -1,7 +1,9 @@
 package com.nextfaze.daggie.rxjava2
 
 import io.reactivex.Flowable
+import io.reactivex.Maybe
 import io.reactivex.Observable
+import io.reactivex.Single
 
 /**
  * Returns a `Flowable` that simply completes upon an error.
@@ -10,7 +12,13 @@ import io.reactivex.Observable
 fun <T : Any> Flowable<T>.onErrorComplete(): Flowable<T> = onErrorResume { Flowable.empty<T>() }
 
 /**
- * Returns a `Observable` that simply completes upon an error.
+ * Returns an `Observable` that simply completes upon an error.
  * @see io.reactivex.Observable.onErrorResumeNext
  */
 fun <T : Any> Observable<T>.onErrorComplete(): Observable<T> = onErrorResume { Observable.empty<T>() }
+
+/**
+ * Returns a `Maybe` that simply completes upon an error.
+ * @see io.reactivex.Observable.onErrorResumeNext
+ */
+fun <T : Any> Single<T>.onErrorComplete(): Maybe<T> = toMaybe().onErrorComplete()
